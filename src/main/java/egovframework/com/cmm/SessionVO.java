1fecd2c39da53a5c3934a2d960a5b28bbaac9872
Chung10Kr <coohg93@hanmail.net>
2022-04-10 13:47:55 +0900

    add Lombok Maven

--- src/main/java/egovframework/com/cmm/SessionVO.java
+++ src/main/java/egovframework/com/cmm/SessionVO.java
@@ -2,6 +2,8 @@ package egovframework.com.cmm;
 
 import java.io.Serializable;
 
+import lombok.Data;
+
 /**
  * 세션 VO 클래스
  * @author 공통서비스 개발팀 박지욱
@@ -18,6 +20,7 @@ import java.io.Serializable;
  *
  *  </pre>
  */
+@Data
 public class SessionVO implements Serializable {
 
 	private static final long serialVersionUID = -2848741427493626376L;
@@ -33,88 +36,4 @@ public class SessionVO implements Serializable {
 	private String orgnztId;
 	/** 고유아이디 */
 	private String uniqId;
-	/**
-	 * sUserId attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getSUserId() {
-		return sUserId;
-	}
-	/**
-	 * sUserId attribute 값을 설정한다.
-	 * @param sUserId String
-	 */
-	public void setSUserId(String userId) {
-		sUserId = userId;
-	}
-	/**
-	 * sUserNm attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getSUserNm() {
-		return sUserNm;
-	}
-	/**
-	 * sUserNm attribute 값을 설정한다.
-	 * @param sUserNm String
-	 */
-	public void setSUserNm(String userNm) {
-		sUserNm = userNm;
-	}
-	/**
-	 * sEmail attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getSEmail() {
-		return sEmail;
-	}
-	/**
-	 * sEmail attribute 값을 설정한다.
-	 * @param sEmail String
-	 */
-	public void setSEmail(String email) {
-		sEmail = email;
-	}
-	/**
-	 * sUserSe attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getSUserSe() {
-		return sUserSe;
-	}
-	/**
-	 * sUserSe attribute 값을 설정한다.
-	 * @param sUserSe String
-	 */
-	public void setSUserSe(String userSe) {
-		sUserSe = userSe;
-	}
-	/**
-	 * orgnztId attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getOrgnztId() {
-		return orgnztId;
-	}
-	/**
-	 * orgnztId attribute 값을 설정한다.
-	 * @param orgnztId String
-	 */
-	public void setOrgnztId(String orgnztId) {
-		this.orgnztId = orgnztId;
-	}
-	/**
-	 * uniqId attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getUniqId() {
-		return uniqId;
-	}
-	/**
-	 * uniqId attribute 값을 설정한다.
-	 * @param uniqId String
-	 */
-	public void setUniqId(String uniqId) {
-		this.uniqId = uniqId;
-	}
 }
