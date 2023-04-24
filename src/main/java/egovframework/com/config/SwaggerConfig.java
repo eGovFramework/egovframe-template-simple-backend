98aebd27a7c94a71b56bb347c684a6d83d4fa75f
kimilguk <boramcom@daum.net>
2023-04-24 14:47:03 +0900

    스웨그 화면상단에 Authorize버튼 추가 및 토큰값 입력하는 Authorization창 구현

--- src/main/java/egovframework/com/config/SwaggerConfig.java
+++ src/main/java/egovframework/com/config/SwaggerConfig.java
@@ -6,11 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//accessToken 입력 화면과 처리 라이브러리 추가(아래4줄)
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.service.*;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -27,8 +30,32 @@ public class SwaggerConfig {
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
-				.build();	
+				.build()
+				.securityContexts(Arrays.asList(securityContext())) // 스웨그에서 컨텐츠 url 접근 시 인증처리를 위한 보안 규칙 호출
+                .securitySchemes(Arrays.asList(apiKey())); // 스웨그 화면상단에 토큰값 입력하는 창 구조 호출, 여기에 배열로 추가 apiKey메서드를 입력가능	
 	}
+	
+	// Authorization창에 Token값 입력 화면 구조 
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+    // 스웨그에서 컨텐츠 url 접근 시 인증처리를 위한 보안 규칙 추가(아래)
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+    // 토큰 인증영역 배열리스트을 반환하는 매서드
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything"); // 인증영역 객체 생성
+        AuthorizationScope[] authorizationScopeArray = new AuthorizationScope[1]; // 빈 배열 인증영역 객체 생성
+        authorizationScopeArray[0] = authorizationScope; // 배열변수에 인증영역 객체 등록
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopeArray)); // 여기에 배열로 추가 SecurityReference객체를 입력가능
+    }
 	
 	public ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
