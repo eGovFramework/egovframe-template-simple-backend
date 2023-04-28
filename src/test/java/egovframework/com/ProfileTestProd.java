891e52d89f124a01c0d52f18661079aa9485dbda
Chung10Kr <coohg93@hanmail.net>
2023-04-28 17:39:19 +0900

    [2023-04-28] - profile Test Code 작성

---
+++ src/test/java/egovframework/com/ProfileTestProd.java
@@ -0,0 +1,41 @@
+package egovframework.com;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * fileName       : ProfileTestProd
+ * author         : crlee
+ * date           : 2023/04/28
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/04/28        crlee       최초 생성
+ */
+@SpringBootTest
+@ActiveProfiles("prod")
+public class ProfileTestProd {
+
+    @Value("${Globals.DbType}")
+    String dbType;
+
+    @Value("${Globals.OsType}")
+    String osType;
+
+    @Value("${logging.rollingpolicy.maxHistory}")
+    String maxHistory;
+    @Test
+    void propertyTest() {
+        Assertions.assertThat(dbType).isEqualTo("hsql");
+        Assertions.assertThat(osType).isEqualTo("LINUX");
+        Assertions.assertThat(maxHistory).isEqualTo("90");
+    }
+}
