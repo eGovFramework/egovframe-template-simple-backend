256fb4661b0a0a24bcebbb0508aa21c16137a514
Chung10Kr <coohg93@hanmail.net>
2023-04-28 17:44:08 +0900

    [2023-04-28] - 불필요 import 제거

--- src/test/java/egovframework/com/ProfileTestDev.java
+++ src/test/java/egovframework/com/ProfileTestDev.java
@@ -2,12 +2,9 @@ package egovframework.com;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * fileName       : ProfileTestDev
