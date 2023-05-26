5b8f16fc0254b2b4cc1c53e41e4bcdec0632167b
Chung10Kr <coohg93@hanmail.net>
2023-05-27 01:57:40 +0900

    [2023-05-27] - 학습 코드 작성

--- src/test/java/egovframework/study/jpa/QueryDslTest.java
+++ src/test/java/egovframework/study/jpa/QueryDslTest.java
@@ -1,10 +1,10 @@
 package egovframework.study.jpa;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import egovframework.study.jpa.domain.Member;
 import egovframework.study.jpa.domain.QMember;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
@@ -25,14 +25,14 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest // 데이터 JPA 테스트 어노테이션
-@Transactional // 트랜잭션 관리 어노테이션
+@DataJpaTest
+@Transactional
 public class QueryDslTest {
 
     @Autowired
-    private TestEntityManager entityManager; // EntityManager를 테스트하기 위한 객체
+    private TestEntityManager entityManager;
 
-    private JPAQueryFactory queryFactory; // Querydsl을 사용하기 위한 QueryFactory
+    private JPAQueryFactory queryFactory;
 
     @BeforeEach
     public void setup() {
@@ -40,16 +40,9 @@ public class QueryDslTest {
     }
 
     @Test
-    public void testQuerydsl() {
-        // Given
-        for (int i=1 ; i<30 ; i++ ){
-            entityManager.persist(Member.builder()
-                    .id("member_id"+i)
-                    .username("member_name"+i)
-                    .age(i)
-                    .build());
-        }
-        // When
+    @DisplayName("QueryDSL로 selectList")
+    public void testSelectList() {
+        insertTestMembers(30);
 
         QMember member = QMember.member;
         List<Member> members = queryFactory
@@ -58,9 +51,62 @@ public class QueryDslTest {
                 .orderBy(member.username.asc())
                 .fetch();
 
-        // Then
         assertThat(members.size()).isEqualTo(6);
         assertThat(members.get(0).getUsername()).isEqualTo("member_name10");
         assertThat(members.get(5).getUsername()).isEqualTo("member_name15");
     }
+
+    @Test
+    @DisplayName("QueryDSL로 selectList - 동적")
+    public void testSelectListDynamic() throws CloneNotSupportedException {
+        insertTestMembers(30);
+
+        QMember member = QMember.member;
+        BooleanBuilder builder = createAgeNameFilter(member, 10);
+
+        List<Member> members = queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
+
+        assertThat(members.size()).isEqualTo(1);
+        assertThat(members.get(0).getUsername()).isEqualTo("member_name10");
+        assertThat(members.get(0).getAge()).isEqualTo(10);
+    }
+    private BooleanBuilder createAgeNameFilter(QMember member, int age) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (age == 10) {
+            builder.and(member.age.eq(10));
+        } else {
+            builder.and(member.age.eq(15)).or(member.username.contains("member_name"));
+        }
+        return builder;
+    }
+    @Test
+    @DisplayName("QueryDSL로 selectOne")
+    public void testSelectOne() {
+        insertTestMembers(5);
+
+        QMember member = QMember.member;
+        Member members = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(3))
+                .fetchOne();
+
+        assertThat(members.getUsername()).isEqualTo("member_name3");
+        assertThat(members.getAge()).isEqualTo(3);
+    }
+
+    private void insertTestMembers(int count) {
+        for (int i = 1; i <= count; i++) {
+            entityManager.persist(Member.builder()
+                    .id("member_id" + i)
+                    .username("member_name" + i)
+                    .age(i)
+                    .build());
+        }
+    }
+
+
 }
