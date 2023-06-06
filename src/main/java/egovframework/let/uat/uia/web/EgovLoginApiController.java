a99b0829683f74964c7df8d7621ad9288ffb4ff7
Chung10Kr <coohg93@hanmail.net>
2023-06-06 18:22:30 +0900

    [2023-06-06] - 불필요 import 제거

--- src/main/java/egovframework/let/uat/uia/web/EgovLoginApiController.java
+++ src/main/java/egovframework/let/uat/uia/web/EgovLoginApiController.java
@@ -6,7 +6,6 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
