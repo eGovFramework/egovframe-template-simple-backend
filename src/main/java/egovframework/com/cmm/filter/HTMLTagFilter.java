20c2bb929558f0929b8ca04fea07f1bb773abe90
dr.hongjae <76645966+dr-hongjae@users.noreply.github.com>
2023-07-20 10:07:06 +0900

    Update HTMLTagFilter.java 주석 생성

--- src/main/java/egovframework/com/cmm/filter/HTMLTagFilter.java
+++ src/main/java/egovframework/com/cmm/filter/HTMLTagFilter.java
@@ -30,17 +30,22 @@ public class HTMLTagFilter implements Filter{
 	@SuppressWarnings("unused")
 	private FilterConfig config;
 
+	// 필터 동작을 처리하는 메서드
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 
+		// 요청을 HTMLTagFilterRequestWrapper로 감싸서 전달합니다.
+        	// 이렇게 함으로써, 요청 파라미터에서 HTML 태그를 제거하는 작업을 수행합니다.
 		chain.doFilter(new HTMLTagFilterRequestWrapper((HttpServletRequest)request), response);
 	}
 
+	// 필터 초기화 메서드 (실행 시 필요한 초기화 작업을 수행합니다.)
 	public void init(FilterConfig config) throws ServletException {
 		this.config = config;
 	}
 
+	// 필터 종료 메서드 (필터 사용 후 작업을 수행합니다.)
 	public void destroy() {
-
+		// 필요한 경우 종료 시 정리 작업을 수행할 수 있습니다.
 	}
 }
