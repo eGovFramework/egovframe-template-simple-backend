914fd5584c82f6a0f7692c1835cfd7eaf7da5147
smile.w.dev <devsmilejwb@gmail.com>
2023-08-09 13:16:47 +0900

    주석 추가

--- src/main/java/egovframework/com/cmm/service/ResultVO.java
+++ src/main/java/egovframework/com/cmm/service/ResultVO.java
@@ -7,6 +7,27 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 
+ * @author : 정완배
+ * @since : 2023. 8. 9.
+ * @version : 1.0
+ *
+ * @package : egovframework.com.cmm.service
+ * @filename : ResultVO.java
+ * @modificationInformation
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *  수정일            수정자             수정내용
+ *  ----------   ----------   ----------------------
+ *  2023. 8. 9.    정완배             주석추가
+ * </pre>
+ *
+ *
+ */
+
 @Schema(description = "응답 객체 VO")
 @Getter
 @Setter
--- src/main/java/egovframework/com/cmm/web/EgovBindingInitializer.java
+++ src/main/java/egovframework/com/cmm/web/EgovBindingInitializer.java
@@ -8,6 +8,26 @@ import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.support.WebBindingInitializer;
 
+/**
+ * 
+ * @author : 정완배
+ * @since : 2023. 8. 9.
+ * @version : 1.0
+ *
+ * @package : egovframework.com.cmm.web
+ * @filename : EgovBindingInitializer.java
+ * @modificationInformation
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *  수정일            수정자             수정내용
+ *  ----------   ----------   ----------------------
+ *  2023. 8. 9.    정완배             주석추가
+ * </pre>
+ *
+ *
+ */
 public class EgovBindingInitializer implements WebBindingInitializer {
 
 	@Override
