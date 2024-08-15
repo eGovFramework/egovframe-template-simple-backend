5c115f5ace4de17319c619db23a0ad93f4412d46
이백행 <dlqorgod@naver.com>
2024-08-15 17:59:38 +0900

    로그인 오류 수정

--- src/main/java/egovframework/com/jwt/EgovJwtTokenUtil.java
+++ src/main/java/egovframework/com/jwt/EgovJwtTokenUtil.java
@@ -24,7 +24,7 @@ public class EgovJwtTokenUtil implements Serializable {
 	public static final long JWT_TOKEN_VALIDITY = (long) ((1 * 60 * 60) / 60) * 60; // 토큰의 유효시간 설정, 기본 60분
 
 	public static final String SECRET_KEY = EgovProperties.getProperty("Globals.jwt.secret");
-  
+
 	// retrieve username from jwt token
 	public String getUserIdFromToken(String token) {
 		Claims claims = getClaimFromToken(token);
@@ -71,6 +71,7 @@ public class EgovJwtTokenUtil implements Serializable {
 		claims.put("userSe", loginVO.getUserSe());
 		claims.put("orgnztId", loginVO.getOrgnztId());
 		claims.put("uniqId", loginVO.getUniqId());
+		claims.put("groupNm", loginVO.getGroupNm());
 		claims.put("type", subject);
 
 		log.debug("===>>> secret = " + SECRET_KEY);
