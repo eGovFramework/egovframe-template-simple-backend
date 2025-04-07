7cc4ddbaafecd14f5255a9799b3c3581c7088301
nirsa <islandtim@naver.com>
2025-04-07 19:38:22 +0900

    Docs(BoardMasterSearchVO) : 게시판 검색 조건 VO에 JavaDoc 주석 추가

--- src/main/java/egovframework/let/cop/bbs/service/BoardMasterSearchVO.java
+++ src/main/java/egovframework/let/cop/bbs/service/BoardMasterSearchVO.java
@@ -4,6 +4,28 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 게시판 검색 조건을 담는 VO 클래스
+ * 게시판 목록 조회 시 사용되는 검색 파라미터들을 구조화하여 전달하기 위한 데이터 객체입니다.
+ * 
+ * - Swagger 문서화 및 파라미터 자동 바인딩 용도로 사용됩니다.
+ * - ModelAttribute 또는 RequestParam 변환 시 검색 조건 처리에 활용됩니다.
+ * 
+ * @author 김재섭(nirsa)
+ * @since 2025.04.06
+ * @version 1.0
+ * @see
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *   
+ *   수정일          수정자         수정내용
+ *   ----------    ------------     -------------------
+ *   2025.04.06    김재섭(nirsa)      최초 생성 
+ *
+ * </pre>
+ */
+
 @Getter
 @Setter
 @Schema(description = "게시판 검색 조건")
