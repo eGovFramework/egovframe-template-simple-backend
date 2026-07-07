a437c8cb5b8253c68cb4ff172c8ab1bcf23c50c1
Eric <leesk55@gmail.com>
2026-07-07 17:31:24 +0900

    chore: 주석 오타 수정 - 메세지→메시지 (let/cop/com/service)

--- src/main/java/egovframework/let/cop/com/service/UserInfVO.java
+++ src/main/java/egovframework/let/cop/com/service/UserInfVO.java
@@ -71,7 +71,7 @@ public class UserInfVO implements Serializable {
     /** 현재페이지 */
     private int pageIndex = 1;
 
-    /** 페이지갯수 */
+    /** 페이지개수 */
     private int pageUnit = 10;
 
     /** 페이지사이즈 */
