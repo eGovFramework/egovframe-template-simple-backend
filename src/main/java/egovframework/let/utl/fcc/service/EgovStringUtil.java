4498daa5dd551db8eaae605b9f0ae6c17bb6f2de
bluejick <bluejick@gmail.com>
2026-09-09 10:14:27 +0900

    fix: EgovStringUtil Javadoc 오타 수정 (더할분자열 → 더할문자열)
    cutString 메서드 2개의 @return 설명에서
    '더할분자열'을 '더할문자열'로 수정합니다.

--- src/main/java/egovframework/let/utl/fcc/service/EgovStringUtil.java
+++ src/main/java/egovframework/let/utl/fcc/service/EgovStringUtil.java
@@ -73,7 +73,7 @@ public class EgovStringUtil {
 	 * @param source 원본 문자열 배열
 	 * @param output 더할문자열
 	 * @param slength 지정길이
-	 * @return 지정길이로 잘라서 더할분자열 합친 문자열
+	 * @return 지정길이로 잘라서 더할문자열 합친 문자열
 	 */
 	public static String cutString(String source, String output, int slength) {
 		String returnVal = null;
@@ -91,7 +91,7 @@ public class EgovStringUtil {
 	 * 문자열이 지정한 길이를 초과했을때 해당 문자열을 삭제하는 메서드
 	 * @param source 원본 문자열 배열
 	 * @param slength 지정길이
-	 * @return 지정길이로 잘라서 더할분자열 합친 문자열
+	 * @return 지정길이로 잘라서 더할문자열 합친 문자열
 	 */
 	public static String cutString(String source, int slength) {
 		String result = null;
